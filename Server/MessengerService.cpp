b0d483e24a01ed85c9df402ae4b1275617beae84
tongstar <68733249+tongmon@users.noreply.github.com>
2023-10-25 10:16:39 +0900

    Update server side logic
    Add todo list
    Modify some DB structure

--- Server/MessengerService.cpp
+++ Server/MessengerService.cpp
@@ -72,17 +72,38 @@ void MessengerService::LoginHandling()
                              });
 }
 
+// client send 형식
+// 보낸 사람 id | 채팅방 id | 보낸 시각 | 컨텐츠 타입 | 채팅 내용
 void MessengerService::MessageHandling()
 {
     std::vector<std::string> parsed;
     boost::split(parsed, m_client_request, boost::is_any_of("|"));
-    std::string sender_id = parsed[0], session_id = parsed[1], content = parsed[2], creator_id, created_date, created_order;
+    std::string sender_id = parsed[0], session_id = parsed[1], send_date = parsed[2], content_type = parsed[3], content = parsed[4];
 
-    parsed.clear();
-    boost::split(parsed, session_id, boost::is_any_of("-"));
-    creator_id = parsed[0], created_date = parsed[1], created_order = parsed[2];
+    // session_id에서 정보 뜯어오는 건데 안쓸거 같음
+    // parsed.clear();
+    // boost::split(parsed, session_id, boost::is_any_of("-"));
+    // std::string creator_id = parsed[0], created_date = parsed[1], created_order = parsed[2];
 
-    soci::rowset<soci::row> rs = (m_sql->prepare << "select participant_id from participant_tb where sessoin_id=:sid",
+    // mongodb에 채팅 내용 업로드
+    using namespace bsoncxx;
+    using namespace bsoncxx::builder;
+
+    auto mongo_client = MongoDBPool::Get().acquire();
+    auto mongo_db = (*mongo_client)["Minigram"];
+    auto mongo_coll = mongo_db[session_id];
+
+    std::istringstream time_in(send_date);
+    std::chrono::system_clock::time_point tp;
+    time_in >> std::chrono::parse("%F %T", tp);
+
+    mongo_coll.insert_one(basic::make_document(basic::kvp("sender_id", sender_id),
+                                               basic::kvp("send_date", types::b_date{tp}),
+                                               basic::kvp("content_type", content_type),
+                                               basic::kvp("content", content)));
+
+    // 채팅 내용 다른 사람에게 전송
+    soci::rowset<soci::row> rs = (m_sql->prepare << "select participant_id from participant_tb where session_id=:sid",
                                   soci::use(session_id, "sid"));
 
     for (soci::rowset<soci::row>::const_iterator it = rs.begin(); it != rs.end(); ++it)
@@ -100,8 +121,9 @@ void MessengerService::MessageHandling()
         auto request_id = m_peer->MakeRequestID();
         m_peer->AsyncConnect(login_ip, login_port, request_id);
 
-        TCPHeader header(TEXTCHAT_CONNECTION_TYPE, content.size());
-        std::string request = header.GetHeaderBuffer() + sender_id + "|" + content;
+        std::string request = sender_id + "|" + session_id + "|" + send_date + "|" + content_type + "|" + content;
+        TCPHeader header(TEXTCHAT_CONNECTION_TYPE, request.size());
+        request = header.GetHeaderBuffer() + request;
 
         m_peer->AsyncWrite(request_id, request, [peer = m_peer](std::shared_ptr<Session> session) -> void {
             if (!session.get() || !session->IsValid())
@@ -110,8 +132,6 @@ void MessengerService::MessageHandling()
             peer->CloseRequest(session->GetID());
         });
     }
-
-    // 해당되는 session_id의 mongodb 정보 수정
 
     delete this;
 }
@@ -122,6 +142,9 @@ void MessengerService::MessageHandling()
 // 날짜 포맷은 chrono의 time_point 형식임
 void MessengerService::ChatRoomListInitHandling()
 {
+    using namespace bsoncxx;
+    using namespace bsoncxx::builder;
+
     std::vector<std::string> parsed;
     std::unordered_map<std::string, std::string> chatroom_recent_date;
 
@@ -166,10 +189,16 @@ void MessengerService::ChatRoomListInitHandling()
         chat_obj["session_img"] = EncodeBase64(img_buffer);
 
         auto mongo_coll = mongo_db[collection_name];
+        auto opts = mongocxx::options::find{};
+        opts.sort(basic::make_document(basic::kvp("send_date", 1)).view());
+        std::unique_ptr<mongocxx::cursor> mongo_cursor;
 
         // 클라이언트에서 해당 채팅방 캐싱을 처음하는 경우(혹은 캐시 파일이 날라갔거나)
+        // 최신 날짜가 맨 뒤로 정렬되게 50개 가져옴
         if (chatroom_recent_date[session_id][0] == '0')
         {
+            opts.limit(50);
+            mongo_cursor = std::make_unique<mongocxx::cursor>(mongo_coll.find({}, opts));
         }
         else
         {
@@ -177,160 +206,50 @@ void MessengerService::ChatRoomListInitHandling()
             std::chrono::system_clock::time_point tp;
             time_in >> std::chrono::parse("%F %T", tp);
 
-            mongo_coll.find(bsoncxx::builder::basic::make_document(bsoncxx::builder::basic::kvp("send_date",
-                                                                                                bsoncxx::builder::basic::make_document(bsoncxx::builder::basic::kvp("$gt",
-                                                                                                                                                                    bsoncxx::types::b_date{tp})))));
+            mongo_cursor = std::make_unique<mongocxx::cursor>(mongo_coll.find(basic::make_document(basic::kvp("send_date",
+                                                                                                              basic::make_document(basic::kvp("$gt",
+                                                                                                                                              types::b_date{tp})))),
+                                                                              opts));
         }
 
-        // 밑에서 부터 mongodb 자체의 sort로 해결 가능이라 지울지 고민중...
+        boost::json::array content_array;
+        for (auto doc : *mongo_cursor)
+        {
+            boost::json::object chat_content;
+            chat_content["sender_id"] = doc["sender_id"].get_string().value;
 
-        // auto mongo_coll = mongo_db[collection_name];
-        // auto root_date_info = mongo_coll.find_one(bsoncxx::builder::basic::make_document(bsoncxx::builder::basic::kvp("date_relation", "root")));
-        //
-        // std::set<std::string> valid_years, valid_months, valid_days;
-        // boost::split(valid_years, root_date_info.value()["valid_year"].get_string().value, boost::is_any_of("|"));
-        //
-        // std::vector<std::string> valid_dates;
-        // boost::json::array content_array;
-        //
-        // // 대화기록이 하나도 없는 경우
-        // if (valid_years.empty())
-        // {
-        //     chat_obj["content"] = content_array;
-        //     chat_room_array.push_back(chat_obj);
-        //     continue;
-        // }
-        //
-        // for (auto y = valid_years.rbegin(); y != valid_years.rend(); y++)
-        // {
-        //     root_date_info = mongo_coll.find_one(bsoncxx::builder::basic::make_document(bsoncxx::builder::basic::kvp("date_relation", "child"),
-        //                                                                                 bsoncxx::builder::basic::kvp("valid_year", *y)));
-        //
-        //     boost::split(valid_months, root_date_info.value()["valid_month"].get_string().value, boost::is_any_of("|"));
-        //     for (auto m = valid_months.rbegin(); m != valid_months.rend(); m++)
-        //     {
-        //         root_date_info = mongo_coll.find_one(bsoncxx::builder::basic::make_document(bsoncxx::builder::basic::kvp("date_relation", "leaf"),
-        //                                                                                     bsoncxx::builder::basic::kvp("valid_year", *y),
-        //                                                                                     bsoncxx::builder::basic::kvp("valid_month", *m)));
-        //
-        //         boost::split(valid_days, root_date_info.value()["valid_day"].get_string().value, boost::is_any_of("|"));
-        //         for (auto d = valid_days.rbegin(); d != valid_days.rend(); d++)
-        //         {
-        //             std::string date = std::format("{}-{}-{}", *y, *m, *d);
-        //             if (recent_date <= date)
-        //             {
-        //                 valid_dates.push_back(date);
-        //             }
-        //             else
-        //             {
-        //                 // 3중 for문 탈출
-        //             }
-        //         }
-        //     }
-        // }
+            std::chrono::system_clock::time_point tp(doc["send_date"].get_date().value);
+            chat_content["send_date"] = std::format("{0:%F %T}", tp);
 
-        // mongo_coll.find_one(bsoncxx::builder::basic::make_document(bsoncxx::builder::basic::kvp("")));
+            chat_content["content_type"] = doc["content_type"].get_string().value;
+            chat_content["content"] = doc["content"].get_string().value;
 
-        // chat_room_path = boost::dll::program_location().parent_path().string() + "/" + creator_id + "/" + session_id;
+            content_array.push_back(chat_content);
+        }
 
-        //// session 정보 json 파일 읽기
-        // std::stringstream str_buf;
-        // std::ifstream session_info(chat_room_path + "/session_info.json");
-        // if (session_info.is_open())
-        //     str_buf << session_info.rdbuf();
-        // session_info.close();
-        //
-        // boost::json::error_code ec;
-        // boost::json::value session_json = boost::json::parse(str_buf.str(), ec);
-        // const boost::json::object &obj = session_json.as_object();
-        //
-        //// session 정보 json에서 필요한 정보 파싱
-        // std::string session_name = boost::json::value_to<std::string>(obj.at("session_name"));
-        // boost::json::array content_array;
-        //
-        // boost::filesystem::path root(chat_room_path);
-        // boost::filesystem::directory_iterator path_it{root};
-        //
-        //// 현재 날짜
-        // auto cur_time_t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-        // struct tm cur_time;
-        // gmtime_s(&cur_time, &cur_time_t); // 윈도우에서만 사용함, thread-safe
-        //
-        //// 가장 최근 날짜로부터 가까운 내용 json 찾음, 일단 3일치만
-        // for (int year = cur_time.tm_year; year >= 1900; year--)
-        //{
-        //     std::string path_year = chat_room_path + "/" + std::to_string(year);
-        //     if (!boost::filesystem::exists(path_year))
-        //         continue;
-        //
-        //    // 해당 년도 폴더가 있으면 하위 달 폴더를 탐색함
-        //    int month = cur_time.tm_mon + 1;
-        //    while (month)
-        //    {
-        //        std::string path_month = path_year + "/" + std::to_string(month--);
-        //        if (!boost::filesystem::exists(path_month))
-        //            continue;
-        //
-        //        int day = cur_time.tm_mday;
-        //        while (day)
-        //        {
-        //            std::string path_day = path_month + "/" + std::to_string(day--) + ".json";
-        //            if (!boost::filesystem::exists(path_day))
-        //                continue;
-        //
-        //            str_buf.clear();
-        //            session_info.open(path_day);
-        //            if (session_info.is_open())
-        //                str_buf << session_info.rdbuf();
-        //            session_info.close();
-        //
-        //            boost::json::object chat_content;
-        //            chat_content["chat_date"] = std::format("{}/{}/{}", year, month + 1, day + 1);
-        //            chat_content["content"] = str_buf.str();
-        //            content_array.push_back(chat_content);
-        //
-        //            if (content_array.size() == 3)
-        //                goto CONTENT_ARRAY_IS_FULL;
-        //        }
-        //    }
-        //}
-
-        // CONTENT_ARRAY_IS_FULL:
-        //     boost::json::object chat_obj;
-        //     chat_obj["creator_id"] = creator_id;
-        //     chat_obj["session_id"] = session_id;
-        //     chat_obj["session_name"] = session_name;
-        //     chat_obj["content"] = content_array;
-        //
-        //    int width, height, channels;
-        //    std::string image_path = chat_room_path + "/session_img.png";
-        //    unsigned char *img = stbi_load(image_path.c_str(), &width, &height, &channels, 0);
-        //    std::string img_buffer(reinterpret_cast<char const *>(img), width * height);
-        //    chat_obj["session_img"] = EncodeBase64(img_buffer);
-        //
-        //    chat_room_array.push_back(chat_obj);
+        chat_obj["content"] = content_array;
+        chat_room_array.push_back(chat_obj);
     }
 
-    // 여기서 부터는 살리셈
-    // boost::json::object chatroom_init_json;
-    // chatroom_init_json["chatroom_init_data"] = chat_room_array;
-    //
-    // // chatroom_init_json에 담긴 json을 client에 전송함
-    // m_request = StrToUtf8(boost::json::serialize(chatroom_init_json));
-    //
-    // TCPHeader header(CHATROOMLIST_INITIAL_TYPE, m_request.size());
-    // m_request = header.GetHeaderBuffer() + m_request;
-    //
-    // boost::asio::async_write(*m_sock,
-    //                          boost::asio::buffer(m_request),
-    //                          [this](const boost::system::error_code &ec, std::size_t bytes_transferred) {
-    //                              if (ec != boost::system::errc::success)
-    //                              {
-    //                                  // write에 이상이 있는 경우
-    //                              }
-    //
-    //                              delete this;
-    //                          });
+    boost::json::object chatroom_init_json;
+    chatroom_init_json["chatroom_init_data"] = chat_room_array;
+
+    // chatroom_init_json에 담긴 json을 client에 전송함
+    m_request = EncodeBase64(StrToUtf8(boost::json::serialize(chatroom_init_json)));
+
+    TCPHeader header(CHATROOMLIST_INITIAL_TYPE, m_request.size());
+    m_request = header.GetHeaderBuffer() + m_request;
+
+    boost::asio::async_write(*m_sock,
+                             boost::asio::buffer(m_request),
+                             [this](const boost::system::error_code &ec, std::size_t bytes_transferred) {
+                                 if (ec != boost::system::errc::success)
+                                 {
+                                     // write에 이상이 있는 경우
+                                 }
+
+                                 delete this;
+                             });
 }
 
 void MessengerService::StartHandling()
