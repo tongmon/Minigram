e6f091f4dfe35d8e79b3a5f9410a8f18fd883ebb
tongstar <68733249+tongmon@users.noreply.github.com>
2023-11-15 13:44:35 +0900

    Working on contactmodel

---
+++ Client/ContactModel.cpp
@@ -0,0 +1,43 @@
+﻿#include "ContactModel.hpp"
+
+ContactModel::ContactModel(QObject *parent)
+    : QAbstractListModel(parent)
+{
+}
+
+int ContactModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+    return m_contacts.size();
+}
+
+QVariant ContactModel::data(const QModelIndex &index, int role) const
+{
+    if (index.row() < 0 && index.row() >= rowCount())
+        return QVariant();
+
+    Contact contact = m_contacts[index.row()];
+    switch (role)
+    {
+    case ID_ROLE:
+        return contact.user_id;
+    case NAME_ROLE:
+        return contact.user_name;
+    case IMG_ROLE:
+        return contact.user_img;
+    case INFO_ROLE:
+        return contact.user_info;
+    default:
+        return QVariant();
+    }
+}
+
+QHash<int, QByteArray> ContactModel::roleNames() const
+{
+    QHash<int, QByteArray> roles;
+    roles[ID_ROLE] = "id";
+    roles[NAME_ROLE] = "name";
+    roles[IMG_ROLE] = "img";
+    roles[INFO_ROLE] = "info";
+    return roles;
+}
---
+++ Client/ContactModel.hpp
@@ -0,0 +1,51 @@
+﻿#ifndef HEADER__FILE__CONTACTMODEL
+#define HEADER__FILE__CONTACTMODEL
+
+#include <QAbstractListModel>
+#include <QMetaType>
+
+#include <string>
+
+struct Contact
+{
+    Q_GADGET
+    Q_PROPERTY(QString name MEMBER user_name)
+    Q_PROPERTY(QString id MEMBER user_id)
+    Q_PROPERTY(QString img MEMBER user_img)
+    Q_PROPERTY(QString info MEMBER user_info)
+
+  public:
+    QString user_id;
+    QString user_name;
+    QString user_img; // base64 png
+    QString user_info;
+
+    Contact(const QString &id = "", const QString &name = "", const QString &base64_img = "", const QString &info = "")
+        : user_id{id}, user_name{name}, user_img{base64_img}, user_info{info}
+    {
+    }
+};
+Q_DECLARE_METATYPE(Contact)
+
+class ContactModel : public QAbstractListModel
+{
+    enum ContactRoles
+    {
+        ID_ROLE = Qt::UserRole + 1,
+        NAME_ROLE,
+        IMG_ROLE,
+        INFO_ROLE
+    };
+    Q_OBJECT
+
+    QList<Contact> m_contacts;
+
+  public:
+    ContactModel(QObject *parent = nullptr);
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+    QHash<int, QByteArray> roleNames() const;
+};
+
+#endif /* HEADER__FILE__CONTACTMODEL */
